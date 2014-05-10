4294a03a2940f42a557b578c541624371306d476
wilbertom <wilbertomorales777@gmail.com>
2014-05-10 10:50:34 -0500

    Remove files not needed to track and add a gitignore

--- build/scripts-3.4/schtask.py
+++
@@ -1,8 +0,0 @@
-import site
-from subprocess import Popen
-
-python_dir = site.getsitepackages()[0]
-site_packages_dir = site.getsitepackages()[1]
-python_cmd = python_dir + "\python.exe" + " " + site_packages_dir + "\selenium_sniper\selenium.py"
-schtasks_cmd = "schtasks /Create /TR" + ''' "''' + python_cmd + '''"''' + " /ST 00:05 /SC DAILY" + " /TN SeleniumSniper"
-Popen(schtasks_cmd)
