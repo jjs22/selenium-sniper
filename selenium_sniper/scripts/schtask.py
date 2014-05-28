c619fd4b01ed26f65e603c9e6412a231a7884479
benjabean1 <benjabean1@gmail.com>
2014-05-28 10:18:15 -0500

    Add batch script for post_install and add schtask.py command to use as variables in schtask.cmd

--- selenium_sniper/scripts/schtask.py
+++ selenium_sniper/scripts/schtask.py
@@ -1,8 +1,17 @@
+# schtask.py
+# Used for variables in schtask.cmd
 import site
-from subprocess import Popen
+import sys
 
 python_dir = site.getsitepackages()[0]
 site_packages_dir = site.getsitepackages()[1]
-python_cmd = python_dir + "\python.exe" + " " + site_packages_dir + "\selenium_sniper\selenium.py"
-schtasks_cmd = "schtasks /Create /TR" + ''' "''' + python_cmd + '''"''' + " /ST 00:05 /SC DAILY" + " /TN SeleniumSniper"
-Popen(schtasks_cmd)
+if sys.argv[1] == "0":
+    print(python_dir)
+if sys.argv[1] == "1":
+    print(site_packages_dir)
+
+print(python_dir)
+print(site_packages_dir)
+# python_cmd = python_dir + "\python.exe" + " " + site_packages_dir + "\selenium_sniper\selenium.py"
+# schtasks_cmd = "schtasks /Create /TR" + ''' "''' + python_cmd + '''"''' + " /ST 00:05 /SC DAILY" + " /TN SeleniumSniper"
+# Popen(schtasks_cmd)
