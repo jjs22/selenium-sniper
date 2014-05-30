cf0296d11f9c1f4666adb2e9d4017fc8e337a9a0
benjabean1 <benjabean1@gmail.com>
2014-05-30 12:33:39 -0500

    Remove unneeded code in schtask.cmd and fix schtask.py for schtask.cmd

--- selenium_sniper/scripts/schtask.cmd
+++ selenium_sniper/scripts/schtask.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
-for /f "delims=" %%a in ('start python schtask.py 0') do @set pydir=%%a
-for /f "delims=" %%a in ('start python schtask.py 1') do @set pysitedir=%%a
+for %%a in ('start python schtask.py 0') do @set pydir=%%a
+for %%a in ('start python schtask.py 1') do @set pysitedir=%%a
 
 @echo on
 schtasks /Create /TR %pydir%\python.exe %pysitedir%\selenium_sniper\selenium.py /ST 00:05 /SC DAILY /TN SeleniumSniper
--- selenium_sniper/scripts/schtask.py
+++ selenium_sniper/scripts/schtask.py
@@ -10,8 +10,8 @@ if sys.argv[1] == "0":
 if sys.argv[1] == "1":
     print(site_packages_dir)
 
-print(python_dir)
-print(site_packages_dir)
+# print(python_dir)
+# print(site_packages_dir)
 # python_cmd = python_dir + "\python.exe" + " " + site_packages_dir + "\selenium_sniper\selenium.py"
 # schtasks_cmd = "schtasks /Create /TR" + ''' "''' + python_cmd + '''"''' + " /ST 00:05 /SC DAILY" + " /TN SeleniumSniper"
 # Popen(schtasks_cmd)
